8c7b3ce603a3b31dab86e465d0440be92cbe4fa0
Leonardo Monsalve <leonardomonsalv@gmail.com>
2019-01-02 05:47:15 +0000

    Add User and Message resources, create a message partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Leo", password: "12345678")
+User.create(username: "Jane", password: "12345678")
+User.create(username: "John", password: "12345678")
+User.create(username: "Victor", password: "12345678")
